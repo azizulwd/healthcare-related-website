927c55d9d5834f3e1e0a2a85ee719be48554611d
azizulwd <azizulislamgd@gmail.com>
2021-10-18 04:07:49 +0600

    header added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../../images/logo.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className="header">
             <div className="container header_inner">
                 <div className="logo">
-                    <img src="" alt="" />
+                    <Link to="/home">
+                        <img src={logo} alt="" />
+                        <h2>HealthCare</h2>
+                    </Link>
+                </div>
+                <div className="navigation_menu">
+                    <Link to="/home">Home</Link>
+                    <Link to="/aboutus">About Us</Link>
+                    <Link to="/shop">Shop</Link>
+                    <Link to="/login">Login</Link>
                 </div>
             </div>
         </div>
