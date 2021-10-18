0270ed4a01949f43cbf391620d22c9b0fd3563e3
azizulwd <azizulislamgd@gmail.com>
2021-10-18 16:20:41 +0600

    banner customize running

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Banner.css';
+import bannerImg from '../../../images/banner.jpg';
+
+const Banner = () => {
+
+    const bannerImgStyle = {
+        width: '100%',
+        height: '500px',
+        backgroundImage: `url(${bannerImg})`
+    }
+
+    return (
+        <div className="banner" style={bannerImgStyle}>
+            <div className="banner_inner">
+                <div>
+                    <h1>THE BEST DOCTOR</h1>
+                    <p>The good news: a healthy lifestyle can help you feel better. Even better, you don’t have to overhaul your entire life overnight. It’s pretty easy to make a couple of small changes that can steer you in the direction of improved well-being. And once you make one change, that success can motivate you to continue to make more positive shifts.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
