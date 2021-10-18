4913b28fd2afb53da8c318c0b47b6199b8761f97
azizulwd <azizulislamgd@gmail.com>
2021-10-18 18:54:36 +0600

    service added

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './Service.css';
 
 const Service = (props) => {
     console.log(props.service);
+    const {img, name, shortDesc} = props.service;
     
     return (
         <div className="service">
-            
+            <div className="service_img">
+                <img src={img} alt="" />
+            </div>
+            <div className="service_details">
+                <h3>{name}</h3>
+                <p>{shortDesc}</p>
+                <button>Read More</button>
+            </div>
         </div>
     );
 };
