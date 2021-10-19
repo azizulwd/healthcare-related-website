8d18445dff7ca6bffa61c62b29122c46845737a0
azizulwd <azizulislamgd@gmail.com>
2021-10-19 14:19:07 +0600

    firebase initialize

---
+++ src/Pages/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCNhmFCDxjtksI_BWLxB8lJL3sFoeVdQ2o",
+    authDomain: "responsive-healthcare.firebaseapp.com",
+    projectId: "responsive-healthcare",
+    storageBucket: "responsive-healthcare.appspot.com",
+    messagingSenderId: "413514122141",
+    appId: "1:413514122141:web:1dd5f463b6dab110736574"
+};
+
+export default firebaseConfig;
