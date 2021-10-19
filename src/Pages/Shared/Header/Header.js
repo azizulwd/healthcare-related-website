59fff4c29141a8d7f6573020b6e94eecff0e410f
azizulwd <azizulislamgd@gmail.com>
2021-10-19 20:53:01 +0600

    update auth issue

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
                     <Link to="/aboutus">About Us</Link>
                     <Link to="/shop">Shop</Link>
                     <span>{user?.displayName}</span>
-                    {isLogin ? <button className="logout_btn" onClick={logOut}>Logout</button> : <Link to="/login">Login</Link>}
+                    {user.email ? <button className="logout_btn" onClick={logOut}>Logout</button> : <Link to="/login">Login</Link>}
                 </div>
             </div>
         </div>
