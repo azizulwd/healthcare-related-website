015ae3f1cf7ec06a47b866dd5fc584dfd03cf836
azizulwd <azizulislamgd@gmail.com>
2021-10-19 19:41:55 +0600

    authentication update

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -34,8 +34,29 @@ const Login = () => {
                 </div>
             </div>
             :
-            <div className="signOut_btn">
-              <button onClick={logOut}>Log out</button>
+            <div className="form">
+                <div className="form_container">
+                    <div className="form_logo">
+                        <img src={loginLogo} alt="" />
+                        <h1>{isLogin ? 'Login' : 'Register'}</h1>
+                    </div>
+                    <form onSubmit={handleRegistration} className="form_input_group">
+                        <p className="error_message">
+                            <span>{error}</span>
+                        </p>
+                        <input onBlur={handleEmail} type="text" placeholder="Email" />
+                        <input onBlur={handlePassword} type="password" placeholder="Password" />
+                        <input onChange={toggleLogin} type="checkbox" id="checkbox" />
+                        <label htmlFor="checkbox"> Already have an account?</label>
+                        <button className="btn_regular">{isLogin ? 'Login' : 'Register'}</button>
+                        <button onClick={resetPasswordEmail} className="btn_regular">Reset Password</button>
+                        <fieldset>
+                            <legend>Or</legend>
+                            <button onClick={signInWithGoogle} className="btn_regular google_btn">{isLogin ? 'Login' : 'Register'} with Google</button>
+                        </fieldset>
+                        
+                    </form>
+                </div>
             </div>
           }
         </div>
