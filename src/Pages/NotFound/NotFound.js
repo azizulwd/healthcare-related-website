f089e08dec096306f5151b14dc61ad665b7cacc4
azizulwd <azizulislamgd@gmail.com>
2021-10-19 22:40:51 +0600

    not found page update

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Not Found 400000000000004</h1>
+        <div className="notFoundPage">
+            <div>
+                <h1 className="text-center notFoundtext">404</h1>
+                <h2 className="fw-bold text-center mb-4">OPPS! THE PAGE NOT FOUND</h2>
+                <p className="text-center fs-3">Sorry, the page you're looking for doesn't exist. If you think <br/> something is broken, report a problem.</p>
+                <div className="returnHome_btn mt-5">
+                    <Link to="/home">RETURN HOME</Link>
+                </div>
+            </div>
         </div>
     );
 };
